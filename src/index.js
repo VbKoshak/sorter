436da41767cb07ea564149d191241f080ad5213e
Vlad_Boltrukanis <boltrukanisvlad@gmail.com>
2018-09-12 22:42:42 +0300

    age comparator         1) 1         2) 2       reverse comparator         √ 1         3) 2       stringify comparator         √ 1         4) 2         5) 3       mixed comparator         6) hard

--- src/index.js
+++ src/index.js
@@ -1,32 +1,91 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this.sorter = [];
+        this.count = 0;
+        this.standart = 1;
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        // your implementation
 
-  at(index) {
-    // your implementation
-  }
+        this.sorter[this.count] = (element);
+        this.count++;
+        //this.sorter.unshift(element);
+    }
 
-  get length() {
-    // your implementation
-  }
+    at(index) {
+        // your implementation
+        return this.sorter[index];
+        // return this.sorter;
+    }
 
-  toArray() {
-    // your implementation
-  }
+    get length() {
+        // your implementation
+        return this.sorter.length;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    toArray() {
+        // your implementation
+        return this.sorter;
+    }
 
-  setComparator(compareFunction) {
-    // your implementation
-    //
-  }
+    sort(indices) {
+        // your implementation
+
+        var temp;
+        indices.sort(function(a, b) {
+            return a - b;
+        });
+        if (this.standart) {
+            for (var j = 0; j < indices.length; j++) {
+                for (var i = 0; i < indices.length - 1; i++) {
+                    if (this.sorter[indices[i]] > this.sorter[indices[i + 1]]) {
+                        temp = this.sorter[indices[i]];
+                        this.sorter[indices[i]] = this.sorter[indices[i + 1]];
+                        this.sorter[indices[i + 1]] = temp;
+                    };
+                };
+            };
+        }
+        else
+        {
+            //this.sortet.sort(this.compare);
+            ////////////
+            for (var j = 0; j < indices.length; j++) {
+                for (var i = 0; i < indices.length - 1; i++) {
+                    if (this.compare(this.sorter[indices[i]],this.sorter[indices[i+1]]) == 1) {
+                        temp = this.sorter[indices[i]];
+                        this.sorter[indices[i]] = this.sorter[indices[i + 1]];
+                        this.sorter[indices[i + 1]] = temp;
+                    };
+                };
+            };
+            ///////////
+            // var temp = [];
+            // for (var i =0; i < indices.length; i++)
+            // {
+            // temp.push[this.sorter[indices[i]]];
+            // }
+            // this.temp.sort(this.compare);
+            // for (var i =0; i < indices.length; i++)
+            // {
+            // this.sorter[indices[i]] = temp[i];
+            // }
+        }
+    }
+
+    setComparator(compareFunction) {
+        // your implementation
+        //this.sorter.sort(compareFunction);
+        /////////
+        // this.compare = new function(a,b)
+        // {
+        //     return compareFunction(a,b);
+        // };
+        /////////
+        this.compare = compa
+        this.standart = 0;
+    }
 }
 
 module.exports = Sorter;
