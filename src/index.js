d64c62b908797bd88ddefe79a5b1ac826c423849
Vlad_Boltrukanis <boltrukanisvlad@gmail.com>
2018-10-03 23:34:26 +0300

    edited

--- src/index.js
+++ src/index.js
@@ -2,89 +2,49 @@ class Sorter {
     constructor() {
         this.sorter = [];
         this.count = 0;
-        this.standart = 1;
+        this.compare = function(a,b)
+        {
+            return a-b;
+        }
     }
 
     add(element) {
-        // your implementation
-
         this.sorter[this.count] = (element);
         this.count++;
-        //this.sorter.unshift(element);
     }
 
     at(index) {
-        // your implementation
         return this.sorter[index];
-        // return this.sorter;
     }
 
     get length() {
-        // your implementation
         return this.sorter.length;
     }
 
     toArray() {
-        // your implementation
         return this.sorter;
     }
 
     sort(indices) {
-        // your implementation
-
-        var temp;
         indices.sort(function(a, b) {
             return a - b;
         });
-        if (this.standart) {
-            for (var j = 0; j < indices.length; j++) {
-                for (var i = 0; i < indices.length - 1; i++) {
-                    if (this.sorter[indices[i]] > this.sorter[indices[i + 1]]) {
-                        temp = this.sorter[indices[i]];
-                        this.sorter[indices[i]] = this.sorter[indices[i + 1]];
-                        this.sorter[indices[i + 1]] = temp;
-                    };
-                };
-            };
-        }
-        else
+
+        let temp = [];
+        let ilength = indices.length;
+        for (let i =0; i < ilength; i++)
         {
-            //this.sortet.sort(this.compare);
-            ////////////
-            // for (var j = 0; j < indices.length; j++) {
-            //     for (var i = 0; i < indices.length - 1; i++) {
-            //         if (this.compare(this.sorter[indices[i]],this.sorter[indices[i+1]]) == 1) {
-            //             temp = this.sorter[indices[i]];
-            //             this.sorter[indices[i]] = this.sorter[indices[i + 1]];
-            //             this.sorter[indices[i + 1]] = temp;
-            //         };
-            //     };
-            // };
-            ///////////
-            var temp = [];
-            for (var i =0; i < indices.length; i++)
-            {
             temp[temp.length]=this.sorter[indices[i]];
-            }
-            temp.sort(this.compare);
-            for (var i =0; i < indices.length; i++)
-            {
+        };
+        temp.sort(this.compare);
+        for (let i =0; i < ilength; i++)
+        {
             this.sorter[indices[i]] = temp[i];
-            }
-        }
+        };
     }
 
     setComparator(compareFunction) {
-        // your implementation
-        //this.sorter.sort(compareFunction);
-        /////////
-        // this.compare = new function(a,b)
-        // {
-        //     return compareFunction(a,b);
-        // };
-        /////////
         this.compare = compareFunction;
-        this.standart = 0;
     }
 }
 
